52715e38a0e55b03f17e4be2a2b899cb1916538a
JuppLCD <lorenzocanovas207@gmail.com>
2022-10-03 16:19:57 -0300

    fix Credentials firebase

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -1,13 +1,13 @@
 import path from 'path';
 import { v4 as uuid } from 'uuid';
 
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, applicationDefault, ServiceAccount } from 'firebase-admin/app';
 import { storage, credential } from 'firebase-admin';
 
 import { CONFIG_ENV } from '../config';
 
 initializeApp({
-	credential: credential.cert(JSON.stringify(CONFIG_ENV.FIREBASE.credential)),
+	credential: credential.cert(CONFIG_ENV.FIREBASE.credential as ServiceAccount),
 });
 const storageRef = storage().bucket(CONFIG_ENV.FIREBASE.bucket);
 
